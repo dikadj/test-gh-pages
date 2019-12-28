5f431b8085ae0cde4a6d2ecfe827493eece17d33
Primadika Dwijantara <primadika.dwijantara@gmail.com>
2019-12-28 12:06:40 +0700

    Updates

---
+++ precache-manifest.1dd4a781487ea1e4f25e32566c50276b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9dc07f0fc9d1ebd6395521bae501136",
+    "url": "/test-gh-pages/index.html"
+  },
+  {
+    "revision": "5dabc7d7661cd08e0df7",
+    "url": "/test-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1c47b489c0d8f9673a22",
+    "url": "/test-gh-pages/static/js/2.53f55327.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test-gh-pages/static/js/2.53f55327.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5dabc7d7661cd08e0df7",
+    "url": "/test-gh-pages/static/js/main.c350bb3a.chunk.js"
+  },
+  {
+    "revision": "8b1f9932dc3267c71f1c",
+    "url": "/test-gh-pages/static/js/runtime-main.29255e3a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
